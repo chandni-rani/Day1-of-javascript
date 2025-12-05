e925653bb08d1ae287d9213281aec228ce326075
chandni-rani <ranichandni055@gmail.com>
2025-12-05 18:10:02 +0530

    Condition check using string

--- part3/conditions.js
+++ part3/conditions.js
@@ -1,5 +1,7 @@
 //Check if anumber is greater than unother number
 
+const { use } = require("react");
+
 let a = 10;
 let b = 5;
 
@@ -7,4 +9,16 @@ if (a > b) {
     console.log("A is greater than B");
 } else {
     console.log("B is greater than a");   
+}
+
+//Check if a string is equal to another string
+
+let username = "Chandni";
+let unotherUsername = "Chanda";
+
+if (username == unotherUsername) {
+    console.log("Usernames match");
+
+} else {
+    console.log("Usernames do not match");
 }
