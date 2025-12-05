52fb9e392d8ecfcfdb94153b5e3cb7ef3fa2c07b
chandni-rani <ranichandni055@gmail.com>
2025-12-05 18:00:00 +0530

    Condition check

---
+++ part3/conditions.js
@@ -0,0 +1,10 @@
+//Check if anumber is greater than unother number
+
+let a = 10;
+let b = 5;
+
+if (a > b) {
+    console.log("A is greater than B");
+} else {
+    console.log("B is greater than a");   
+}
