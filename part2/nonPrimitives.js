45af387f949c3f89e09ea967dfc4fa402b6d2d4f
chandni-rani <ranichandni055@gmail.com>
2025-12-05 17:41:37 +0530

    Done Non-Primitives

---
+++ part2/nonPrimitives.js
@@ -0,0 +1,9 @@
+const username = {firstname: "chandni", isLoggedin: true};
+
+username.firstname = "Anjali";
+username.lastname = "Sharma";
+
+console.log(username.firstname);
+console.log(username.lastname); 
+console.log(username);
+console.log(typeof username); // "object"
