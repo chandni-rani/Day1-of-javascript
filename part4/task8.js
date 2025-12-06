1248ad09329031d62c1a6aea290ab246c0539397
chandni-rani <ranichandni055@gmail.com>
2025-12-06 18:01:12 +0530

    Done

---
+++ part4/task8.js
@@ -0,0 +1,9 @@
+/* You Have two arrays named `europeanCities` contain "Paris", "Rome" and `asianCities` containe "Tokiyo" Bangkok.
+Marge these two arrays into a new array named "worldCities". */
+
+let europeanCities = ["Paris", "Rome"];
+let asianCities = ["Tokiyo", "Bangkok"];
+
+let worldCities = europeanCities.concat(asianCities);
+
+console.log(worldCities);
