5787df034e61adc6ffafca190375d28eb378181b
chandni-rani <ranichandni055@gmail.com>
2025-12-06 18:14:38 +0530

    Done

---
+++ part4/task10.js
@@ -0,0 +1,8 @@
+/* You have an array named `cityBucketList` containing "New York", "Los Angeles", "Chicago", "Houston", "Phoenix".
+Check if "Chaicago" is in the array and store the result in a variable named `isChicagoInList`. */
+
+let cityBucketList = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+
+let isChicagoInList = cityBucketList.includes("Chicago");
+
+console.log(isChicagoInList);
